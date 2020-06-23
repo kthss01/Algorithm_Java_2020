f91d3944a8194820122b4ceb897612fcef7d9e8b
김태훈 <kthss01@naver.com>
2020-06-23 21:59:07 +0900

    Java Practice

---
+++ Algorithm_Java_2020/src/Datatypes.java
@@ -0,0 +1,72 @@
+
+// Java Datatypes
+
+/*
+    Java has 8 primitive data types: char, boolean, byte, short, int , long, float, and double
+    For thie exercies, integer values (bytes, short, int, and long)
+    byte 8-bit sigend integer.
+    short 16-bit sigend integer
+    int 32-bit sigend integer
+    long 64-bit sigend integer
+    
+    Given ans input integer, you must determine which primitive data types are capable of properly storing that input
+    
+    Input
+    integer T test cases
+    integer n which can be arbitrarily large or small.
+    
+    Output
+    n can be fitted in:
+    * dataType
+    
+    If there is more than one appropriate data type, print each one on its own line and order them by size 
+        (byte < short < int < long)
+    if the number cannot be stored in one of the four aforementioned(앞서 말한) primitives, print the line:
+        n can't be fitted anywhere.
+    
+    long long - BigInteger 이용
+        https://www.acmicpc.net/blog/view/3
+    BigIntegers compare
+        https://riptutorial.com/ko/java/example/4918/bigintegers-%EB%B9%84%EA%B5%90%ED%95%98%EA%B8%B0
+    https://pridiot.tistory.com/62
+*/
+
+import java.util.*;
+import java.math.*;
+
+public class Datatypes {
+
+	public static void main(String[] args) {
+
+		Scanner scan = new Scanner(System.in);
+
+		int T = scan.nextInt();
+
+		for (int j = 0; j < T; j++) {
+			BigInteger bI = scan.nextBigInteger();
+
+			if (!bI.equals(BigInteger.valueOf(bI.longValue())))
+				System.out.printf("%s can't be fitted anywhere.\n", bI.toString());
+			else {
+				System.out.printf("%s can be fitted in:\n", bI.toString());
+
+				if (bI.equals(BigInteger.valueOf(bI.intValue()))) {
+					int integer = bI.intValue();
+					if (integer == (short) integer) {
+						short s = (short) integer;
+						if (s == (byte) s) {
+							System.out.println("* byte");
+						}
+						System.out.println("* short");
+					}
+					System.out.println("* int");
+				}
+				System.out.println("* long");
+			}
+		}
+
+		scan.close();
+
+	}
+
+}
---
+++ Algorithm_Java_2020/src/StaticInitializerBlock.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+
+/**
+ * @author Kay Static initialization blocks (정적 초기화 블럭) are executed when the
+ *         class is loaded, and you can initialize static variables in those
+ *         blocks.
+ * 
+ *         parallelogram 평행 사변형
+ * 
+ *         Task area of a parallelogram with breadth B and height H if B <= 0 or
+ *         H <= 0 output shoudle be "java.lang.Exception: breadth and height
+ *         must be positive" without quotes
+ * 
+ *         -100 <= B, H <= 100
+ */
+public class StaticInitializerBlock {
+
+	static int B, H;
+	static boolean flag;
+
+	static {
+		java.util.Scanner scan = new Scanner(System.in);
+
+		B = scan.nextInt();
+		H = scan.nextInt();
+
+		flag = B > 0 && H > 0;
+		if (!flag)
+			System.out.println("java.lang.Exception: Breadth and height must be positive");
+
+		scan.close();
+	}
+
+	public static void main(String[] args) {
+		if (flag) {
+			int area = B * H;
+			System.out.println(area);
+		}
+	}
+
+}
