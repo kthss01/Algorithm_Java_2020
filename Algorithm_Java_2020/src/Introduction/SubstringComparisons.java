bdcae77f21061ac465ccfcfc859e25a84fe3f618
kthss01 <kthss01@naver.com>
2020-07-01 01:09:37 +0900

    학원에서 못풀었던 부분문자열 문제 풀기

--- Algorithm_Java_2020/src/Introduction/SubstringComparisons.java
+++ Algorithm_Java_2020/src/Introduction/SubstringComparisons.java
@@ -35,20 +35,14 @@ public class SubstringComparisons {
 		String smallest = "";
 		String largest = "";
 
-		String[] arr = new String[s.length() - k + 1];
-		
 		/*
-		 * 문자열의 길이가 최대 1000이면
-		 * 가능한 부분 문자열은
-		 * 1자리 : 1000-0 개
-		 * 2자리 : 1000-1 개
-		 * 3자리 : 1000-2 개
-		 * ...
-		 * 1000자리 : 1000-999개
-		 * 총합 : 1부터 1000까지의 합 = (1000)(1000+1)/2 = 500500 임
+		 * 문자열의 길이가 최대 1000이면 가능한 부분 문자열은 1자리 : 1000-0 개 2자리 : 1000-1 개 3자리 : 1000-2 개
+		 * ... 1000자리 : 1000-999개 총합 : 1부터 1000까지의 합 = (1000)(1000+1)/2 = 500500 임
 		 * http://melonicedlatte.com/algorithm/2018/08/12/230648.html
 		 */
-		
+
+		String[] arr = new String[s.length() - k + 1];
+
 		int cnt = 0;
 		for (int i = 0; i < s.length(); i++) {
 			if (i + k - 1 >= s.length())
@@ -56,11 +50,20 @@ public class SubstringComparisons {
 			arr[cnt] = s.substring(i, i + k);
 			cnt++;
 		}
-		
+
+		smallest = arr[0];
+		largest = arr[0];
+		for (int i = 1; i < arr.length; i++) {
+			if (arr[i].compareTo(smallest) < 0)
+				smallest = arr[i];
+			if (arr[i].compareTo(largest) > 0)
+				largest = arr[i];
+		}
+
 		// import 불가 문제였음
 		// ArrayList, Collections 못씀
 		// compareTo로 비교해야하는 거 같음
-		
+
 //		ArrayList<String> arr = new ArrayList();
 //
 //		for (int i = 0; i < s.length(); i++) {
@@ -75,10 +78,10 @@ public class SubstringComparisons {
 //		largest = arr.get(arr.size() - 1);
 
 		// test
-		for (String string : arr) {
-			System.out.print(string + " ");
-		}
-		System.out.println();
+//		for (String string : arr) {
+//			System.out.print(string + " ");
+//		}
+//		System.out.println();
 
 		System.out.println(smallest + "\n" + largest);
 	}
